1b8e2ebdfaf267bcbd52b315a5df3f5eec7f82de
Elina <elinapark0818@gmail.com>
2021-10-01 22:44:06 +0900

    Home update

--- src/components/navbar/navControl.js
+++ src/components/navbar/navControl.js
@@ -6,10 +6,12 @@ import './navControl.css';
 export default function NavControl() {
   const [navSize, setnavSize] = useState("5rem");
   const [navColor, setnavColor] = useState("transparent");
+  const [logo, setLogo] = useState("0")
   
   const listenScrollEvent = () => {
-    window.scrollY > 100 ? setnavColor("#1b1b1b") : setnavColor("transparent");
+    window.scrollY > 550 ? setnavColor("#1b1b1b") : setnavColor("transparent");
     window.scrollY > 100 ? setnavSize("5rem") : setnavSize("5rem");
+    window.scrollY > 100 ? setLogo("1") : setLogo("0");
   };
 
   useEffect(() => {
@@ -27,7 +29,10 @@ export default function NavControl() {
           height: navSize,
           transition: "all 1s"
         }}>
-          <div className="navLogo">
+          <div className="navLogo" style={{
+            opacity: logo,
+            transition: "all 1s"
+          }}>
             Wave.
           </div>
         <ul className="navList">
--- src/components/page/home/home.jsx
+++ src/components/page/home/home.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './home.module.css';
 import { useSpring, animated } from 'react-spring'
 
 export default function Home() {
 
-  const flip = useState(false);
+  
 
-  const flipUp = useSpring({
-    from: { opacity: 0, marginTop: -50},
-    to: { opacity: flip ? 1 : 0, marginTop: 0 },
-  })
+  const [fadeOut, setFadeOut] = useState("0")
+
+  const listenScrollEvent = () => {
+    window.scrollY > 100 ? setFadeOut("0") : setFadeOut("1");
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+    return () => {
+      window.removeEventListener("scroll", listenScrollEvent);
+    };
+  }, []);
 
 
   return (
@@ -21,12 +29,21 @@ export default function Home() {
       />
       <div className={styles.titleWrap}>
         <animated.h1 className={styles.title}
-          style={flipUp}
+          style={{
+            opacity: fadeOut,
+            transition: "all 1s"
+          }}
           >
           Wave.
           <br/>Wherever
           <br/>Changeable
         </animated.h1>
+        <p className={styles.homePs}
+          style={{
+            opacity: fadeOut,
+            transition: "all 1s"
+          }}
+          >변화무쌍한 파도처럼 언제나 대응 가능한 와타시와 괴발자</p>
       </div>
     </div>
   )
